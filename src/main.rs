c297c3a90a5f4c01160a308c16f0363c12e335cf
Jonathan Kim <jkim8192@gmail.com>
2017-05-11 19:39:47 -0400

    Allow configuration of log filename

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,17 @@ extern crate log4rs;
 extern crate postgres;
 extern crate serenity;
 
-use log4rs::encode::pattern::PatternEncoder;
-use std::process;
-
 const PROGRAM_NAME: &'static str = env!("CARGO_PKG_NAME");
 const PROGRAM_VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const LOG_FILE: &str = "emojistats.log";
+const DEFAULT_LOG_FILENAME: &str = "emojistats.log";
 
 fn init_logging() {
+    let filename = dotenv::var("ES_LOG_FILENAME").unwrap_or(DEFAULT_LOG_FILENAME.to_string());
+
     let file = log4rs::append::file::FileAppender::builder()
-            .encoder(Box::new(PatternEncoder::new(
+            .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
                     "{d(%Y-%m-%d %H:%M:%S %Z)(local)}: {h({l})}: {m}{n}")))
-            .build(LOG_FILE)
+            .build(filename)
             .expect("Failed to create log file");
 
     let appender = log4rs::config::Appender::builder().build("emojistats", Box::new(file));
@@ -29,9 +28,11 @@ fn init_logging() {
         .expect("Failed to build logging configuration");
 
     log4rs::init_config(config).expect("Failed to initialize logger");
-
 }
 
 fn main() {
+    dotenv::dotenv().ok();
     init_logging();
+
+    info!("Started {} v{}", PROGRAM_NAME, PROGRAM_VERSION);
 }
